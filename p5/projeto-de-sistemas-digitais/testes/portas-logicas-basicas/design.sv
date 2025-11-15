51c6e7c56805c946362b82e706fc738d206e9ff9
Miguel Ryan Dantas de Freitas <miguelryanfreitas@gmail.com>
2025-11-15 11:05:00 -0300

    eslint reclamando aí

--- p5/projeto-de-sistemas-digitais/testes/portas-logicas-basicas/design.sv
+++ p5/projeto-de-sistemas-digitais/testes/portas-logicas-basicas/design.sv
@@ -1,6 +1,6 @@
 module porta_and (
-	input logic a, b,
-	output logic result
+    input logic a, b,
+    output logic result
 );
   assign result = a & b;
 endmodule
@@ -14,37 +14,37 @@ module porta_or (
 endmodule
 
 module porta_not (
-	input logic a,
-  	output logic result
+    input logic a,
+    output logic result
 );
   assign result = ~a;
 endmodule
 
 module porta_nand(
-	input logic a, b,
-  	output logic result
+    input logic a, b,
+    output logic result
 );
   assign result = ~(a & b);
 endmodule
 
 module porta_nor(
-	input logic a, b,
-  	output logic result
+    input logic a, b,
+    output logic result
 );
   assign result = ~(a | b);
 endmodule
 
 module porta_xor(
-	input logic a, b,
-  	output logic result
+    input logic a, b,
+    output logic result
 );
   assign result = a ^ b;
 
 endmodule
 
 module porta_xnor(
-	input logic a, b,
-  	output logic result
+    input logic a, b,
+    output logic result
 );
   assign result = ~(a ^ b);
 endmodule
